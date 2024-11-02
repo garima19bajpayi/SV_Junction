2dbf720f22e5057d9ac4ce2ac344a1fe8a33b216
Garima Bajpayi <garimabajpayi19@gmail.com>
2024-11-02 23:38:22 +0530

    added even_parity_checker

---
+++ Even_parity_checker/design.sv
@@ -0,0 +1,10 @@
+module even_parity_checker (
+    input  logic [3:0] data_in,
+    input  logic       parity_in,
+    output logic       error
+);
+
+    assign error = (data_in[0] ^ data_in[1] ^ data_in[2] ^ data_in[3] ^ parity_in);
+
+endmodule
+
